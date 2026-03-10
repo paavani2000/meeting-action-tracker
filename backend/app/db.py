0185710ec9098a4ce183a0e6bc334e0757f48388
paavani2000 <paavanir@usc.edu>
2026-03-09 21:24:47 -0700

    cleanup

--- backend/app/db.py
+++ backend/app/db.py
@@ -2,10 +2,14 @@ from sqlalchemy import create_engine, Column, Integer, String, Text, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from datetime import datetime
+import os
+from dotenv import load_dotenv
 
-DATABASE_URL="postgresql+psycopg2://postgres:Hithapavani%239700@localhost:5432/meeting_tracker"
+load_dotenv()
 
-
+DATABASE_URL = os.environ.get("DATABASE_URL")
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL is not set in .env")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
